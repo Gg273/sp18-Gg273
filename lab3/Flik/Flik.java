faae2342be4a4ce2937b3c87b9a3809d09e46a3e
Gg273 <2627828030@qq.com>
2023-07-06 23:30:12 +0800

    finish the lab3

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,17 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        /**
+         * I don't why the compare result will be false when a and b both greater equal to 128,
+         * but this is the error, and you can change it to a.equal(b).
+         * After search in stackoverflow about Integer for few minutes.
+         * The Integer is a reference type, and when use the range between -128 to 127, the Integer will
+         * return a default set instance while running the java code; otherwise, will create a new instance, and this
+         * is comparing the two reference instances, ex: new Integer(128) == new Integer(128) and will be false.
+         * and could be use -Djava.lang.Integer.IntegerCache.high=999 the system property to change the default range.
+         * the better way is use the equals() method. ex: a.equals(b);
+         * */
+        return a.equals(b);
+        //return a == b;
     }
 }
--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,4 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class HorribleSteve {
+
+    @Test
+    public void testIsSameNumber() {
+        int i = 126;
+        int j = 126;
+        assertTrue(Flik.isSameNumber(i, j));
+        i = 127;
+        j = 127;
+        assertTrue(Flik.isSameNumber(i, j));
+        i = 129;
+        j = 129;
+        assertTrue(Flik.isSameNumber(i, j));
+    }
     public static void main(String [] args) {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
